b6824f7dd20757e0c4dcec51e76e393c0c3b70e3
LBECFIT <87069036+LBECFIT@users.noreply.github.com>
2022-06-30 10:29:45 +0700

    Update app.py

--- streamlit_app_2/app.py
+++ streamlit_app_2/app.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import streamlit as st
+import plotly_express as px
 
 st.title('new app test')
 
-#df = pd.read_csv('df_rf3.csv',sep=',')
-#st.dataframe(df)
+df = pd.read_csv('df_rf3.csv')
+st.dataframe(df)
