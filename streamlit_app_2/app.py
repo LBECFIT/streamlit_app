878cf963d57f954b69614667da2b3cb67dd5dc6c
LBECFIT <87069036+LBECFIT@users.noreply.github.com>
2022-06-30 10:42:44 +0700

    Update app.py

--- streamlit_app_2/app.py
+++ streamlit_app_2/app.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 st.title('new app test')
 
-df = pd.read_csv('github.com/LBECFIT/streamlit_app/blob/main/streamlit_app_2/df_rf3.csv')
+df = pd.read_csv('df_all_merged.csv')
 st.dataframe(df)
