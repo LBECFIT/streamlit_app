de7aead165b05748d975df4f5e14ee46fffbc1fb
LBECFIT <87069036+LBECFIT@users.noreply.github.com>
2022-06-30 10:39:10 +0700

    Update app.py

--- streamlit_app_2/app.py
+++ streamlit_app_2/app.py
@@ -3,5 +3,5 @@ import streamlit as st
 
 st.title('new app test')
 
-df = pd.read_csv('df_rf3.csv')
+df = pd.read_csv('https://github.com/LBECFIT/streamlit_app/blob/main/streamlit_app_2/df_rf3.csv')
 st.dataframe(df)
